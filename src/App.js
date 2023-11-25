4510341f07d598781ad6b803e3dbff511224a095
monkeymr014 <monkeymr014@gmail.com>
2023-11-25 18:13:13 +0100

    chakra

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { Flex, Heading, Highlight  } from '@chakra-ui/react'
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <Flex alignItems='center' justifyContent='center' m={2}>
+      <Heading><Highlight
+    query='zadań'
+    styles={{ px: '3', py: '1', rounded: 'full', bg: 'blue.300' }}
+  >Lista zadań</Highlight></Heading>
+    </Flex>
   );
 }
 
